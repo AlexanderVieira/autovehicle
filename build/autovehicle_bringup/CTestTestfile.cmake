e84b3688539da578eaef81d51d7514cd2b7e9d55
alexandervieira <alexander.silva@al.infnet.edu.br>
2021-03-15 03:30:49 -0300

    Implementaçao static_transformation_publisher, bem como refatoraçao setup e servo Laser Scan.

---
+++ build/autovehicle_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros_ws/src/autovehicle_bringup
+# Build directory: /home/ros_ws/build/autovehicle_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/autovehicle_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros_ws/src/autovehicle_navigation
+# Build directory: /home/ros_ws/build/autovehicle_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
